44381a301bb969ae77833832b8721b2e8fb6aa1d
Proxypepe <sasha_golovin_95@bk.ru>
2022-08-04 19:27:20 +0300

    added todos reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import UserList from "./conponents/userList";
+import TodoList from "./conponents/todoList";
 
 const App = () => {
     return (
         <div>
             <UserList/>
+            <hr/>
+            <TodoList/>
         </div>
     );
 };
